257cbe5aef7bebb1c0df658d85573ea2bcde9092
matheusakio <matheus.akio2@gmail.com>
2022-03-30 09:46:01 -0300

    feat: creation components

--- react/react-component-lib/src/components/Icon/Icon.stories.tsx
+++ react/react-component-lib/src/components/Icon/Icon.stories.tsx
@@ -1,23 +1,25 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-import Icon from './index';
+import Icon from "./index";
 
 export default {
-    title: 'Icon',
-    component: Icon,
-    argTypes: {
-        size: {
-            options: ['small', 'normal', 'medium', 'large'],
-            control: { type: 'select'},
-            description: 'Icon size',
-        },
-        name: {
-            defaultValue: 'delete',
-            description: 'Icon name',
-        }
+  title: "Icon",
+  component: Icon,
+  argTypes: {
+    size: {
+      options: ["small", "normal", "medium", "large"],
+      control: { type: "select" },
+      description: "Icon size",
     },
+    name: {
+      options: ["delete", "arrowUp", "arrowDown"],
+      control: { type: "select" },
+      defaultValue: "delete",
+      description: "Icon name",
+    },
+  },
 } as ComponentMeta<typeof Icon>;
 
 export const Examples: ComponentStory<typeof Icon> = (args) => (
-    <Icon {...args}/>
+  <Icon {...args} />
 );
--- react/react-component-lib/src/components/Icon/index.tsx
+++ react/react-component-lib/src/components/Icon/index.tsx
@@ -1,36 +1,40 @@
-import classNames from 'classnames/bind';
+import classNames from "classnames/bind";
 
-import { ThemeSizes, ThemeSizeValues } from 'theme';
-import { ReactComponent as DeleteIcon} from 'assets/icons/delete.svg';
-
+import { ThemeSizes, ThemeSizeValues } from "theme";
+import { ReactComponent as DeleteIcon } from "assets/icons/delete.svg";
+import { ReactComponent as ArrowDownIcon } from "assets/icons/arrow-down.svg";
+import { ReactComponent as ArrowUpIcon } from "assets/icons/arrow-up.svg";
+import { ReactComponent as UploadIcon } from "assets/icons/upload.svg";
 
 const IconExporter = {
-    'delete': DeleteIcon,
+  delete: DeleteIcon,
+  arrowDown: ArrowDownIcon,
+  arrowUp: ArrowUpIcon,
+  upload: UploadIcon,
 };
 
 export type IconType = keyof typeof IconExporter;
 
 interface IIcon {
-    name: IconType;
-    size?: ThemeSizes;
-};
+  name: IconType;
+  size?: ThemeSizes;
+  color?: string;
+}
 
-const Icon: React.FC<IIcon> = ({ 
-    name,
-    size = 'normal',
-    ...rest
-}) => {
-    const IconJsx = IconExporter[name];
-    const w = ThemeSizeValues[size];
-    const iconClasses = classNames({
-        'icon': true,
-    });
+const Icon: React.FC<IIcon> = ({ name, size = "normal", color, ...rest }) => {
+  const IconJsx = IconExporter[name];
+  const w = ThemeSizeValues[size];
+  const iconClasses = classNames({
+    icon: true,
+  });
 
-    return (
-        <span className={iconClasses} {...rest}>
-            {IconJsx ? (<IconJsx style={{width: w, height: w}} />) : null}
-        </span>
-    );
+  return (
+    <span className={iconClasses} {...rest}>
+      {IconJsx ? (
+        <IconJsx style={{ width: w, height: w, color: color }} />
+      ) : null}
+    </span>
+  );
 };
 
 export default Icon;
